f18aa3e1eaf9a3c9a4820cf7b221acc105aff520
italo <italogustavo207@gmail.com>
2019-10-06 18:06:20 -0300

    version 1

---
+++ Fluxo de Caixa/src/fluxo/de/caixa/FluxoDeCaixa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fluxo.de.caixa;
+
+/**
+ *
+ * @author Italo
+ */
+public class FluxoDeCaixa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
